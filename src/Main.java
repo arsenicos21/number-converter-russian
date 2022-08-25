00702487a36c46a47330c29a47bce42a99e3a1bb
Arseniy Ageev <arseniy.north@gmail.com>
2022-08-26 01:05:10 +0300

    Int converted to Long. Increased the maximum value of numbers. Fixed bugs

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,17 @@ public class Main {
     public static void main(String[] args) {
         NumberInWords numberInWords = new NumberInWords();
 
-        System.out.println("1768 = " + numberInWords.convert(1768));
-        System.out.println("176845 = " + numberInWords.convert(176845));
-        System.out.println("1691753 = " + numberInWords.convert(1691753));
-        System.out.println("48563249 = " + numberInWords.convert(48563249));
-        System.out.println("0 = " + numberInWords.convert(0));
-        System.out.println("3 = " + numberInWords.convert(3));
-        System.out.println("220 = " + numberInWords.convert(220));
+        System.out.println("1_768 = " + numberInWords.convert(1_768L));
+        System.out.println("176_845 = " + numberInWords.convert(176_845L));
+        System.out.println("1_691_753 = " + numberInWords.convert(1_691_753L));
+        System.out.println("48_563_249 = " + numberInWords.convert(48_563_249L));
+        System.out.println("0 = " + numberInWords.convert(0L));
+        System.out.println("3 = " + numberInWords.convert(3L));
+        System.out.println("220 = " + numberInWords.convert(220L));
+        System.out.println("20_000 = " + numberInWords.convert(20_000L));
+        System.out.println("12_654_875_112_986 = " + numberInWords.convert(12_654_875_112_986L));
+        System.out.println("54_835_112_122 = " + numberInWords.convert(54_835_112_122L));
+        System.out.println("456_777_815_112_900 = " + numberInWords.convert(456_777_815_112_900L));
+        System.out.println("830_110_300 = " + numberInWords.convert(830_110_300L));
     }
 }
--- src/NumberInWords.java
+++ src/NumberInWords.java
@@ -1,6 +1,20 @@
 
 public class NumberInWords {
 
+    private static final String[] trillionNames = {
+            "",
+            " триллион",
+            " триллиона",
+            " триллионов"
+    };
+
+    private static final String[] billionNames = {
+            "",
+            " миллиард",
+            " миллиарда",
+            " миллиардов"
+    };
+
     private static final String[] millionNames = {
             "",
             " миллион",
@@ -90,59 +104,80 @@ public class NumberInWords {
     public NumberInWords() {
     }
 
-    private static String maleConvertLessThanOneThousand(int number) {
+    private static String maleConvertLessThanOneThousand(long number) {
         String soFar;
 
         if (number % 100 < 20) {
-            soFar = maleNumNames[number % 100];
+            soFar = maleNumNames[Math.toIntExact(number % 100)];
+            number /= 100;
+
+            soFar = hundredNames[Math.toIntExact(number % 10)] + soFar;
         } else {
-            soFar = maleNumNames[number % 10];
+            soFar = maleNumNames[Math.toIntExact(number % 10)];
             number /= 10;
 
-            soFar = tensNames[number % 10] + soFar;
+            soFar = tensNames[Math.toIntExact(number % 10)] + soFar;
             number /= 10;
 
-            soFar = hundredNames[number % 10] + soFar;
+            soFar = hundredNames[Math.toIntExact(number % 10)] + soFar;
         }
         return soFar;
     }
 
-    private static String femaleConvertLessThanOneThousand(int number) {
+    private static String femaleConvertLessThanOneThousand(long number) {
         String soFar;
 
         if (number % 100 < 20) {
-            soFar = femaleNumNames[number % 100];
+            soFar = femaleNumNames[Math.toIntExact(number % 100)];
+            number /= 100;
+
+            soFar = hundredNames[Math.toIntExact(number % 10)] + soFar;
         } else {
-            soFar = femaleNumNames[number % 10];
+            soFar = femaleNumNames[Math.toIntExact(number % 10)];
             number /= 10;
 
-            soFar = tensNames[number % 10] + soFar;
+            soFar = tensNames[Math.toIntExact(number % 10)] + soFar;
             number /= 10;
 
-            soFar = hundredNames[number % 10] + soFar;
+            soFar = hundredNames[Math.toIntExact(number % 10)] + soFar;
         }
         return soFar;
     }
 
-    public static String convert (int number) {
+    public static String convert (long number) {
         if (number == 0) {
             return "ноль";
         }
 
-        int numberOfMillions = number / 1000000;
-        int numberOfThousands = (number - (numberOfMillions * 1000000)) / 1000;
-        int numberOfNum = number % 1000;
+        long numberOfTrillions = number / 1_000_000_000_000L;
+        long numberOfBillions = (number - (numberOfTrillions * 1_000_000_000_000L)) / 1_000_000_000L;
+        long numberOfMillions = (number - ((numberOfTrillions * 1_000_000_000_000L)
+                + (numberOfBillions * 1_000_000_000L)))/ 1_000_000L;
+        long numberOfThousands = (number - ((numberOfTrillions * 1_000_000_000_000L)
+                + (numberOfBillions * 1_000_000_000L) + (numberOfMillions * 1_000_000L))) / 1000L;
+        long numberOfNum = number % 1000;
 
         String result;
         StringBuilder stringBuilder = new StringBuilder();
 
-        if (number < 1000) {
+        if (number < 1_000L) {
             stringBuilder.append(maleConvertLessThanOneThousand(number));
-        } else if (number > 1000 && number < 1000000) {
+        } else if (number > 1_000L && number < 1_000_000L) {
             stringBuilder.append(femaleConvertLessThanOneThousand(numberOfThousands)
-                    + thousandWordReturner(number) + maleConvertLessThanOneThousand(numberOfNum));
-        } else if (number >= 1000000) {
+                    + thousandWordReturner(Math.toIntExact(number)) + maleConvertLessThanOneThousand(numberOfNum));
+        } else if (number >= 1_000_000L && number < 1_000_000_000L) {
             stringBuilder.append(maleConvertLessThanOneThousand(numberOfMillions) + millionWordReturner(number)
+                    + femaleConvertLessThanOneThousand(numberOfThousands) + thousandWordReturner(number)
+                    + maleConvertLessThanOneThousand(numberOfNum));
+        } else if (number >= 1_000_000_000L && number < 1_000_000_000_000L) {
+            stringBuilder.append(maleConvertLessThanOneThousand(numberOfBillions) + billionWordReturner(number)
+                    + maleConvertLessThanOneThousand(numberOfMillions) + millionWordReturner(number)
+                    + femaleConvertLessThanOneThousand(numberOfThousands) + thousandWordReturner(number)
+                    + maleConvertLessThanOneThousand(numberOfNum));
+        } else if (number >= 1_000_000_000_000L) {
+            stringBuilder.append(maleConvertLessThanOneThousand(numberOfTrillions) + trillionWordReturner(number)
+                    + maleConvertLessThanOneThousand(numberOfBillions) + billionWordReturner(number)
+                    + maleConvertLessThanOneThousand(numberOfMillions) + millionWordReturner(number)
                     + femaleConvertLessThanOneThousand(numberOfThousands) + thousandWordReturner(number)
                     + maleConvertLessThanOneThousand(numberOfNum));
         }
@@ -150,8 +185,62 @@ public class NumberInWords {
        return result;
     }
 
-    public static String millionWordReturner(int number) {
-        int numberOfMillions = number / 1000000;
+    public static String trillionWordReturner(long number) {
+        long numberOfTrillions = number / 1_000_000_000_000L;
+        String trillionWord = null;
+        if (numberOfTrillions % 100 < 20) {
+            if (numberOfTrillions == 1) {
+                return trillionNames[1];
+            } else if (numberOfTrillions <= 4) {
+                return trillionNames[2];
+            } else if (numberOfTrillions >= 5) {
+                return trillionNames[3];
+            }
+        } else {
+            if (numberOfTrillions % 10 == 0) {
+                return trillionNames[3];
+            }else if (numberOfTrillions % 10 == 1) {
+                return trillionNames[1];
+            } else if (numberOfTrillions % 10 <= 4) {
+                return trillionNames[2];
+            } else if (numberOfTrillions % 10 >= 5) {
+                return trillionNames[3];
+            }
+        }
+        return trillionWord;
+    }
+
+    public static String billionWordReturner(long number) {
+        long numberOfTrillions = number / 1_000_000_000_000L;
+        long numberOfBillions = (number - (numberOfTrillions * 1_000_000_000_000L)) / 1_000_000_000L;
+        String billionWord = null;
+        if (numberOfBillions % 100 < 20) {
+            if (numberOfBillions == 1) {
+                return billionNames[1];
+            } else if (numberOfBillions <= 4) {
+                return billionNames[2];
+            } else if (numberOfBillions >= 5) {
+                return billionNames[3];
+            }
+        } else {
+            if (numberOfBillions % 10 == 0) {
+                return billionNames[3];
+            } else if (numberOfBillions % 10 == 1) {
+                return billionNames[1];
+            } else if (numberOfBillions % 10 <= 4) {
+                return billionNames[2];
+            } else if (numberOfBillions % 10 >= 5) {
+                return billionNames[3];
+            }
+        }
+        return billionWord;
+    }
+
+    public static String millionWordReturner(long number) {
+        long numberOfTrillions = number / 1_000_000_000_000L;
+        long numberOfBillions = (number - (numberOfTrillions * 1_000_000_000_000L)) / 1_000_000_000L;
+        long numberOfMillions = (number - ((numberOfTrillions * 1_000_000_000_000L)
+                + (numberOfBillions * 1_000_000_000L)))/ 1_000_000L;
         String millionWord = null;
         if (numberOfMillions % 100 < 20) {
             if (numberOfMillions == 1) {
@@ -162,7 +251,9 @@ public class NumberInWords {
                 return millionNames[3];
             }
         } else {
-            if (numberOfMillions % 10 == 1) {
+            if (numberOfMillions % 10 == 0) {
+                return millionNames[3];
+            } else if (numberOfMillions % 10 == 1) {
                 return millionNames[1];
             } else if (numberOfMillions % 10 <= 4) {
                 return millionNames[2];
@@ -173,9 +264,13 @@ public class NumberInWords {
         return millionWord;
     }
 
-    public static String thousandWordReturner(int number) {
-        int numberOfMillions = number / 1000000;
-        int numberOfThousands = (number - (numberOfMillions * 1000000)) / 1000;
+    public static String thousandWordReturner(long number) {
+        long numberOfTrillions = number / 1_000_000_000_000L;
+        long numberOfBillions = (number - (numberOfTrillions * 1_000_000_000_000L)) / 1_000_000_000L;
+        long numberOfMillions = (number - ((numberOfTrillions * 1_000_000_000_000L)
+                + (numberOfBillions * 1_000_000_000L)))/ 1_000_000L;
+        long numberOfThousands = (number - ((numberOfTrillions * 1_000_000_000_000L)
+                + (numberOfBillions * 1_000_000_000L) + (numberOfMillions * 1_000_000L))) / 1000L;
         String thousandWord = null;
         if (numberOfThousands % 100 < 20) {
             if (numberOfThousands == 1) {
@@ -186,7 +281,9 @@ public class NumberInWords {
                 return thousandNames[3];
             }
         } else {
-            if (numberOfThousands % 10 == 1) {
+            if (numberOfThousands % 10 == 0) {
+                return thousandNames[3];
+            }else if (numberOfThousands % 10 == 1) {
                 return thousandNames[1];
             } else if (numberOfThousands % 10 <= 4) {
                 return thousandNames[2];
